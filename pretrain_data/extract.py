9c347570ad07ef9aa7acecabfb8b2cd599cedba4
Riroaki <2569126376@qq.com>
2020-05-17 18:51:21 +0800

    fix extracting bugs
    bugs referenced in https://github.com/thunlp/ERNIE/issues/54

--- pretrain_data/extract.py
+++ pretrain_data/extract.py
@@ -2,8 +2,10 @@ from bs4 import BeautifulSoup
 import sys
 from urllib import parse
 import os
+import logging
 from multiprocessing import Pool
 
+logging.basicConfig(level=logging.DEBUG)
 input_folder = "pretrain_data/output"
 
 file_list = []
@@ -11,21 +13,21 @@ for path, _, filenames in os.walk(input_folder):
     for filename in filenames:
         file_list.append(os.path.join(path, filename))
 
+
 def run_proc(idx, n, file_list):
-    for i in range(len(file_list)):
-        if i % n == idx:
-            input_name = file_list[i]
-            print(input_name)
-            target = input_name.replace(input_folder, "pretrain_data/ann")
-            folder = '/'.join(target.split('/')[:-1])
-            if not os.path.exists(folder):
-                os.makedirs(folder)
-            
-            soup = BeautifulSoup(open(input_name), features="html5lib")
-            docs = soup.find_all('doc')
+    for input_name in file_list[idx::n]:
+        target = input_name.replace(input_folder, "pretrain_data/ann")
+        folder = '/'.join(target.split('/')[:-1])
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        elif os.path.exists(target):
+            # Already done parsing
+            continue
 
-            fout = open(target, "w")
+        soup = BeautifulSoup(open(input_name), features="html.parser")
+        docs = soup.find_all('doc')
 
+        with open(target, 'w') as fout:
             for doc in docs:
                 content = doc.get_text(" sepsepsep ")
                 while content[0] == "\n":
@@ -33,17 +35,21 @@ def run_proc(idx, n, file_list):
                 content = [x.strip() for x in content.split("\n")]
                 content = "".join(content[1:])
 
-                lookup = [(x.get_text().strip(), parse.unquote(x.get('href'))) for x in doc.find_all("a")]
-                lookup = "[_end_]".join(["[_map_]".join(x) for x in lookup])
+                try:
+                    lookup = [(x.get_text().strip(), parse.unquote(x.get('href')))
+                              for x in doc.find_all("a")]
+                    lookup = "[_end_]".join(
+                        ["[_map_]".join(x) for x in lookup])
+                except Exception as e:
+                    logging.warning(
+                        'Error {} when parsing file {}'.format(str(e), input_name))
                 fout.write(content+"[_end_]"+lookup+"\n")
+        logging.info('Finished {}'.format(target))
 
-            fout.close()
-
-import sys
 
 n = int(sys.argv[1])
 p = Pool(n)
 for i in range(n):
-    p.apply_async(run_proc, args=(i,n, file_list))
+    p.apply_async(run_proc, args=(i, n, file_list))
 p.close()
 p.join()
